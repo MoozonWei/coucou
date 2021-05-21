156822a2c1174a473483e4bfc09d9cdd912245c7
kksy <1361350111@qq.com>
2021-05-21 15:47:56 +0800

    npm support, rewrite by lin-ui:https://doc.mini.talelin.com, change to new branch dev-lin-ui

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,72 +7,140 @@ Page({
   data: {
     "navList": [{
       "id": 0,
-      "name": "推荐"
+      "name": "推荐",
+      "dataList" : "recommedList"
     }, {
       "id": 1,
-      "name": "关注"
+      "name": "关注",
+      "dataList" : "followList"
     }, {
       "id": 2,
-      "name": "学习"
+      "name": "学习",
+      "dataList" : "learnList"
     }, {
       "id": 3,
-      "name": "运动"
+      "name": "运动",
+      "dataList": "sportsList"
     }, {
       "id": 4,
-      "name": "考研"
+      "name": "考研",
+      "dataList": "graduateList"
     }, {
       "id": 5,
-      "name": "读书"
+      "name": "读书",
+      "dataList" : "bookList"
     }, {
       "id": 6,
-      "name": "电影"
+      "name": "电影",
+      "dataList" : "movieList"
     }, {
       "id": 7,
-      "name": "TEST"
+      "name": "TEST",
+      "dataList": "testList"
     }, {
       "id": 8,
-      "name": "TEST"
+      "name": "TEST",
+      "dataList": "testList"
     }, {
       "id": 9,
-      "name": "TEST"
+      "name": "TEST",
+      "dataList": "testList"
     }],
-    "itemId": 0,
+    "recommedList" : [{
+      "id" : 0,
+      "name" : "科幻"
+    },{
+      "id" : 1,
+      "name" : "奇幻"
+    },{
+      "id" : 2,
+      "name" : "爱情"
+    },{
+      "id" : 3,
+      "name" : "动作"
+    },{
+      "id" : 4,
+      "name" : "悬疑"
+    },{
+      "id" : 5,
+      "name" : "战争"
+    },{
+      "id" : 6,
+      "name" : "喜剧"
+    }],
+    "followList" : [{
+      "id" : 0,
+      "name" : "Hello"
+    }
+    ],
+    "learnList" : [{
+      "id" : 0,
+      "name" : "Learn"
+    }],
+    "sportsList": [{
+      "id" : 1,
+      "name" : "乒乓球"
+    },{
+      "id" : 2,
+      "name" : "羽毛球"
+    },{
+      "id" : 3,
+      "name" : "网球"
+    },{
+      "id" : 4,
+      "name" : "篮球"
+    },{
+      "id" : 5,
+      "name" : "足球"
+    },{
+      "id" : 6,
+      "name" : "排球"
+    },{
+      "id" : 7,
+      "name" : "跑步"
+    }],
+    "graduateList" : [],
+    "bookList" :[],
+    "movieList" : [],
+    "testList" : [],
+    "currList" : [],
     "scrollLeft": 0
   },
 
+  /**
+   * 导航栏tab点击事件，切换当前数据
+   * @param {Object} key - 导航栏当前标签对象
+   */
+  changeTabs(key) {
+    // console.log(key)
+    let currId = key.detail.activeKey
+    let dataMap = this.data.navList[currId].dataList
 
-  ItemSelected(event) {
-    let query = wx.createSelectorQuery()
-    let id = event.currentTarget.id
-    console.log(id)
-    let viewWidth = 0
-    query.select('#' + id).boundingClientRect(function (rect) {
-      viewWidth = rect.width
-    }).exec(() => {
-      let offsetLeft = event.currentTarget.offsetLeft
-      let sL = offsetLeft - this.data.scrollViewWidth / 2 + viewWidth / 2
-      this.setData({
-        scrollLeft: sL,
-        itemId: event.currentTarget.dataset.id
-      })
+    // console.log(currId)
+    // console.log(dataMap)
+
+    let temp = this.data[dataMap]
+    this.setData({
+      currList: temp
     })
-
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
+   * 初始化当前列表为推荐列表
    */
   onReady: function () {
-    wx.createSelectorQuery().select(".navScroll").boundingClientRect((rect) => {
-      this.data.scrollViewWidth = Math.round(rect.width)
-    }).exec()
+    let rec = this.data.recommedList
+    this.setData({
+      currList: rec
+    })
   },
 
   /**
