4733d4e7ec6fea9c1bdf1dbc48bbbc9e7077ace3
Silvengs <1361350111@qq.com>
2021-05-25 19:56:16 +0800

    feat(index): basic index page, map choose location support

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,22 @@
 // index.js
 // 获取应用实例
-const app = getApp(); // 得到app.js里面的函数和数据
 
-Page({})
+Page({
+  data: {
+
+  },
+
+  chooseMap() {
+    const key = 'QFSBZ-PWGKR-CL6WT-WA6GE-R2G27-PMF7K'; //使用在腾讯位置服务申请的key
+    const referer = 'mapDemo'; //调用插件的app的名称
+    const location = JSON.stringify({
+      latitude: 39.89631551,
+      longitude: 116.323459711
+    });
+    const category = '生活服务,娱乐休闲';
+
+    wx.navigateTo({
+      url: 'plugin://chooseLocation/index?key=' + key + '&referer=' + referer + '&location=' + location + '&category=' + category
+    });
+  }
+})
